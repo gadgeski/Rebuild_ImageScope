368898e3298470f044d256f56d7e37784f1ca0da
gadgeski <xtwh.ce5bil@gmail.com>
2025-02-23 16:13:35 +0900

    Initial Commit

---
+++ improvement_ImageScope/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  improvement_ImageScope
+//
+//  Created by Dev Tech on 2025/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ improvement_ImageScope/improvement_ImageScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  improvement_ImageScopeApp.swift
+//  improvement_ImageScope
+//
+//  Created by Dev Tech on 2025/02/23.
+//
+
+import SwiftUI
+
+@main
+struct improvement_ImageScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
